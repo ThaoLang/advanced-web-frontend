aafd08e82506c04b7ac7d7ed7f17d2383c266df3
LHKN <79393909+LHKN@users.noreply.github.com>
2023-11-10 12:53:51 +0700

    feat: handle select option in user profile

--- src/component/ProfileForm.tsx
+++ src/component/ProfileForm.tsx
@@ -11,7 +11,7 @@ export default function ProfileForm(props: ProfileFormProp) {
   const [emailProxy, setEmailProxy] = useState(props.email);
   const [usernameProxy, setUsernameProxy] = useState(props.username);
   const [isEditable, setIsEditable] = useState(false);
-  const [option, setOption] = useState("");
+  const [option, setOption] = useState("button2");
 
   const toggleEdit = async () => {
     setIsEditable(!isEditable);
@@ -23,14 +23,14 @@ export default function ProfileForm(props: ProfileFormProp) {
   };
 
   // use effect to change section?
-  const handleOptionClick = async (option:string) => {
-    setOption(option); // default
-  }
+  const handleOptionClick = async (option: string) => {
+    setOption(option);
+  };
 
   return (
-    <div>
-      <div className="flex flex-row">
-        <div className="mr-10 mt-20 h-full w-full mx-auto inline-block align-middle">
+    <div className="m-20">
+      <div className="flex flex-row justify-center">
+        <div className="mr-10 mt-28 inline-block align-middle">
           <img
             src="https://api.lorem.space/image/face?w=120&h=120&hash=bart89fe"
             className="rounded-full w-40 "
@@ -111,9 +111,74 @@ export default function ProfileForm(props: ProfileFormProp) {
       </div>
       <div className="divider">ABOUT ME</div>
 
+      <div
+        className={`mr-10 mt-20 mx-auto inline-block align-left ${
+          option === "button1" ? "visible" : "hidden"
+        }`}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+        vulputate dictum sagittis. Cras vulputate sit amet lorem sit amet
+        aliquet. Integer hendrerit lobortis sem in mollis. Donec eget luctus
+        nunc. Fusce congue elit quis ipsum porta, suscipit tincidunt dolor
+        sodales. Praesent accumsan nulla sit amet metus laoreet scelerisque.
+        Integer blandit fringilla lorem quis mattis. Phasellus egestas gravida
+        convallis. Integer molestie, sapien vitae accumsan fermentum, mi erat
+        sodales nunc, nec elementum sem mi nec justo. Nulla venenatis
+        condimentum congue. Nunc quis urna elit. Nunc condimentum dapibus lacus,
+        eu tristique risus rhoncus eget. Fusce ut consequat lorem. Suspendisse
+        potenti. Proin vitae enim sed dolor fermentum consequat. Fusce libero
+        risus, lobortis id ante ultrices, accumsan finibus diam. Nam tempor ante
+        nulla, vel cursus leo rutrum vitae. Ut odio ligula, semper eget lectus
+        ac, euismod mattis nunc. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Praesent congue nisl rutrum, malesuada justo vitae,
+        porttitor nisi. Suspendisse a leo sed felis luctus pellentesque id ut
+        sapien.
+      </div>
+
+      <div
+        className={`mr-10 mt-20 mx-auto inline-block align-left ${
+          option === "button2" ? "visible" : "hidden"
+        }`}
+      >
+        Donec in leo diam. Pellentesque habitant morbi tristique senectus et
+        netus et malesuada fames ac turpis egestas. Vivamus in fringilla ex. Sed
+        lacus nisi, molestie id ligula vitae, bibendum ullamcorper erat.
+        Pellentesque pellentesque aliquam metus a gravida. Fusce non metus
+        ultrices, sagittis risus ac, tincidunt nisi. Aenean pulvinar, mauris ut
+        eleifend scelerisque, purus tortor viverra nisi, id porta mauris sem eu
+        justo. Maecenas pulvinar sem a dui accumsan, quis laoreet risus
+        sollicitudin. Aliquam id consequat sem. Praesent id condimentum velit.
+        Integer auctor sit amet urna ut egestas. Nam sit amet turpis et dolor
+        consequat consequat. Duis mi justo, iaculis nec scelerisque ac, aliquam
+        sed purus.
+      </div>
+
+      <div
+        className={`mr-10 mt-20 mx-auto inline-block align-left ${
+          option === "button3" ? "visible" : "hidden"
+        }`}
+      >
+        Vestibulum non eleifend augue. Maecenas efficitur molestie turpis eget
+        lobortis. Quisque rutrum vitae lectus id semper. Morbi rutrum vitae
+        neque vitae facilisis. Cras sagittis nisl pretium mattis posuere.
+        Curabitur pellentesque neque nibh, non gravida lacus fermentum eget.
+        Nullam vel imperdiet nunc. Vestibulum volutpat hendrerit ex sed luctus.
+        Nulla facilisi. Cras euismod nibh enim, in feugiat metus eleifend vitae.
+        Morbi vestibulum nec nulla in auctor. Sed id sapien interdum, dictum
+        sapien eu, rhoncus leo. Etiam sollicitudin ut ligula ut vestibulum.
+        Suspendisse accumsan placerat quam. In id enim nunc. Maecenas vulputate
+        sem eget pretium ultricies. Duis porta scelerisque urna, placerat luctus
+        libero cursus quis. Nulla pharetra dignissim augue faucibus efficitur.
+        Donec varius eget arcu vel tristique. Vivamus ac quam at metus molestie
+        laoreet vel non nibh. Etiam in commodo tellus, vel venenatis ipsum.
+      </div>
+
       {/* <div className="flex w-full flex-row align-middle justify-around"> */}
       <div className="btm-nav">
-        <button id="button1" onClick={() => handleOptionClick("")}>
+        <button
+          className={`${option === "button1" ? "disabled" : "active"}`}
+          onClick={() => handleOptionClick("button1")}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -130,7 +195,10 @@ export default function ProfileForm(props: ProfileFormProp) {
           </svg>
           <span className="btm-nav-label">Comments</span>
         </button>
-        <button className="active" id="button2" onClick={() => handleOptionClick("")}>
+        <button
+          className={`${option === "button2" ? "disabled" : "active"}`}
+          onClick={() => handleOptionClick("button2")}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -147,7 +215,10 @@ export default function ProfileForm(props: ProfileFormProp) {
           </svg>
           <span className="btm-nav-label">Bio</span>
         </button>
-        <button id="button3" onClick={() => handleOptionClick("")}>
+        <button
+          className={`${option === "button3" ? "disabled" : "active"}`}
+          onClick={() => handleOptionClick("button3")}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
