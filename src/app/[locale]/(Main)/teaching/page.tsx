d0489a197701bb699bf04cb36ad41eab0629b994
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2023-11-24 14:41:22 +0700

    Merge branch 'master' of https://github.com/ThaoLang/advanced-web

---
+++ src/app/[locale]/(Main)/teaching/page.tsx
@@ -0,0 +1,6 @@
+import CreateAssignmentModal from "@/component/CreateAssignmentModal";
+import React from "react";
+
+export default function page() {
+  return <div></div>;
+}
--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth } from "@/context/AuthContext";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 export default function HomePage() {
-    const auth = useAuth();
-    return (
-        auth.user ? 
-        <div>
-            Welcome back! {auth.user.username}
-        </div>
-        : <div></div>
-    )
+  const t = useTranslations("LandingPage");
+  const auth = useAuth();
+  return auth.user ? (
+    <div>
+      <label>Welcome back! {auth.user.username}</label>
+    </div>
+  ) : (
+    <div></div>
+  );
 }
