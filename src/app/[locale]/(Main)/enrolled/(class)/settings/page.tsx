1dc39157ae9a0ba0d22145bcc237cf125ef0598b
LHKN <79393909+LHKN@users.noreply.github.com>
2023-11-24 15:04:33 +0700

    chore: temporary pages

---
+++ src/app/[locale]/(Main)/enrolled/(class)/settings/page.tsx
@@ -0,0 +1,12 @@
+// "use client";
+import React, {  } from "react";
+
+export default function Settings() {
+  
+
+  return (
+    <div className="flex flex-col justify-center space-x-2 h-fit">
+      
+    </div>
+  );
+}
