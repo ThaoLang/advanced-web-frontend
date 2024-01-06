ef3816e23c9d22957e3301fcc45cb5e2ed7e5f20
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2024-01-06 17:01:31 +0700

    fixL fix " Error: React Hook "useEffect" is called conditionally"

--- src/app/[locale]/(Main)/enrolled/[classId]/detail/page.tsx
+++ src/app/[locale]/(Main)/enrolled/[classId]/detail/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Banner from "@/component/classItem/detail/Banner";
+import { useAuth } from "@/context/AuthContext";
 import { ClassType } from "@/model/ClassType";
 import { UserType } from "@/model/UserType";
 import axios from "axios";
@@ -9,49 +10,49 @@ import React, { useEffect, useState } from "react";
 
 export default function DetailEnrolledClass() {
   const t = useTranslations("Tabs");
+  const auth = useAuth();
   const [isAvailableStudentList, setIsAvailable] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const { classId } = useParams();
   const [classInfo, setClassInfo] = useState<ClassType>();
-  const savedUser = localStorage.getItem("user");
-  let currentUser: UserType;
-  if (savedUser) {
-    currentUser = JSON.parse(savedUser);
-    const handleModal = () => {
-      console.log("Modal changed");
-      setShowModal(!showModal);
-    };
+  // const savedUser = localStorage.getItem("user");
+  // let currentUser: UserType;
+  // if (savedUser) {
+  //   currentUser = JSON.parse(savedUser);
+  const handleModal = () => {
+    console.log("Modal changed");
+    setShowModal(!showModal);
+  };
 
-    useEffect(() => {
-      axios
-        .get(`${process.env.NEXT_PUBLIC_BACKEND_PREFIX}classes/${classId}`, {
-          headers: {
-            Authorization: `Bearer ${currentUser?.access_token}`,
-          },
-        })
-        .then((response) => {
-          console.log("Response", response.data);
-          setClassInfo(response.data);
-          console.log("Response class", classInfo);
-        })
-        .catch((error) => {
-          console.error("Error fetching classes:", error);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BACKEND_PREFIX}classes/${classId}`, {
+        headers: {
+          Authorization: `Bearer ${auth.user?.access_token}`,
+        },
+      })
+      .then((response) => {
+        console.log("Response", response.data);
+        setClassInfo(response.data);
+        console.log("Response class", classInfo);
+      })
+      .catch((error) => {
+        console.error("Error fetching classes:", error);
+      });
+  }, []);
 
-    return (
-      <div className="flex justify-center items-center mx-auto">
-        {classInfo && (
-          <Banner
-            id={classInfo._id}
-            imageUrl="https://static.vecteezy.com/system/resources/previews/011/005/174/original/creative-education-background-with-school-supplies-vector.jpg"
-            name={classInfo.name}
-            description={classInfo.description}
-            inviteUrl={classInfo.invite_url}
-            classCode={classInfo.class_code}
-          />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className="flex justify-center items-center mx-auto">
+      {classInfo && (
+        <Banner
+          id={classInfo._id}
+          imageUrl="https://static.vecteezy.com/system/resources/previews/011/005/174/original/creative-education-background-with-school-supplies-vector.jpg"
+          name={classInfo.name}
+          description={classInfo.description}
+          inviteUrl={classInfo.invite_url}
+          classCode={classInfo.class_code}
+        />
+      )}
+    </div>
+  );
 }
