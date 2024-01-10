9b5ec01ed5c9b678212b596a78a6dcd14a387810
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2024-01-10 14:55:27 +0700

    chore: add ban error message

--- src/app/[locale]/(Main)/page.tsx
+++ src/app/[locale]/(Main)/page.tsx
@@ -5,9 +5,15 @@ import LandingPage from "@/component/LandingPage";
 import HomePage from "@/component/HomePage";
 import { useAuth } from "@/context/AuthContext";
 import CredentialError from "@/component/admin/CredentialError";
+import { toast, ToastContainer } from "react-toastify";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
   const auth = useAuth();
+  const t = useTranslations("HomePage");
+  const isBanned = () => {
+    toast.error(t("ban_error"));
+  };
   useEffect(() => {
     const savedUser = localStorage.getItem("user");
     if (savedUser !== null) {
@@ -23,6 +29,18 @@ export default function Home() {
       ) : (
         <LandingPage />
       )}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 }
