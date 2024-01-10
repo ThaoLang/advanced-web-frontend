c3b32abc0a0887e442326fa8a74ab70f9abdf944
MiwaUS7605 <minhquang9802@gmail.com>
2024-01-10 16:37:08 +0700

    fix: logout when navigate to login page from RestrictedPage

--- src/component/RestrictedPage.tsx
+++ src/component/RestrictedPage.tsx
@@ -1,16 +1,26 @@
 import { useAuth } from "@/context/AuthContext";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function RestrictedPage() {
+    const auth = useAuth();
+
+    const handleLogout = () => {
+        auth.logout("user");
+    }
+
+    useEffect(() => {
+        //Fetch the current user
+    },[auth.user])
+
     return (
         <React.Fragment>
             <div className="flex flex-col items-center justify-center h-screen">
                 <div className="font-bold text-9xl">403 Forbidden</div>
                 <div className="font-semibold text-4xl">Access denied...</div>
                 <div className="mt-2 text-2xl">You don&apos;t have permission to access this page</div>
-                <Link href="/auth">
+                <Link href="/auth" onClick={handleLogout}>
                     <div className="mt-2 text-lg text-primary">
                         Go to Login Page
                     </div>
